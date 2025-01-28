8ddf3f406bfb6fa1cd85737bc25266168e76cc8e
char2s <char2s@outlook.com>
2025-01-28 16:13:03 -0500

    produce as many warnings as possible

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Test12Component } from './test12/test12.component';
+import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3/test3.component';
+import { Test4Component } from './test4/test4.component';
+import { Test5Component } from './test5/test5.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Test12Component,
+    Test2Component,
+    Test3Component,
+    Test4Component,
+    Test5Component,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'issue-apphosting-build-output';
