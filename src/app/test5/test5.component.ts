7edb0061bca04b1c0622613d8db80dd6174e28bf
char2s <char2s@outlook.com>
2025-01-28 16:24:17 -0500

    add even more warnings

--- src/app/test5/test5.component.ts
+++ src/app/test5/test5.component.ts
@@ -4,8 +4,27 @@ import { Component } from '@angular/core';
   selector: 'app-test5',
   imports: [],
   templateUrl: './test5.component.html',
-  styleUrl: './test5.component.css'
+  styleUrls: [
+    './test5.component.css',
+    './test5.component.1.css',
+    './test5.component.2.css',
+    './test5.component.3.css',
+    './test5.component.4.css',
+    './test5.component.5.css',
+    './test5.component.6.css',
+    './test5.component.7.css',
+    './test5.component.8.css',
+    './test5.component.9.css',
+    './test5.component.10.css',
+    './test5.component.11.css',
+    './test5.component.12.css',
+    './test5.component.13.css',
+    './test5.component.14.css',
+    './test5.component.15.css',
+    './test5.component.16.css',
+    './test5.component.17.css',
+    './test5.component.18.css',
+    './test5.component.19.css',
+  ],
 })
-export class Test5Component {
-
-}
+export class Test5Component {}
